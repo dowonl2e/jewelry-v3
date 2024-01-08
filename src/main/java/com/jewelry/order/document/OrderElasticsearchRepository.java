be4a4ac6b94b2e00c8a15f6cadfefee515592a88
dowonl2e <dierwdod@gmail.com>
2024-01-08 14:28:10 +0900

    Elasticsearch 주석

--- src/main/java/com/jewelry/order/document/OrderElasticsearchRepository.java
+++ src/main/java/com/jewelry/order/document/OrderElasticsearchRepository.java
@@ -1,6 +1,6 @@
 package com.jewelry.order.document;
 
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface OrderElasticsearchRepository extends ElasticsearchRepository<OrderSearchDoc, Long> {
+public interface OrderElasticsearchRepository
+    // extends ElasticsearchRepository<OrderSearchDoc, Long>
+{
 }
