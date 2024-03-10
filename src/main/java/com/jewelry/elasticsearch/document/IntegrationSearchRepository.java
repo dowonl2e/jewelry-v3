27acff90d8f7a3949c9562636612a6bcc48f6a4e
dowonl2e <dierwdod@gmail.com>
2024-03-10 18:04:41 +0900

    Elasticsearch 구성 제거

--- src/main/java/com/jewelry/elasticsearch/document/IntegrationSearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jewelry.elasticsearch.document;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface IntegrationSearchRepository extends ElasticsearchRepository<IntegrationSearchDoc, String> {
-
-}
